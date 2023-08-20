ec9a2d2c674abc08a4c0f2647f76cc96dbc163c5
Bread3838 <lucasr3103@gmail.com>
2023-08-20 16:21:40 +0200

    Changed the entire website

---
+++ js/changing-headline.js
@@ -0,0 +1,22 @@
+const changingHeadline = document.getElementById("changing-headline");
+
+const contentArray = ["Web Developer", "Programmer", "UX Designer"];
+const colorArray = ["#6eb468", "#d66fc3", "#eca116"];
+let index = 0
+
+function changeHeadline() {
+    changingHeadline.style.animation = "none";
+    changingHeadline.offsetHeight;
+    changingHeadline.style.animation = null;
+    changingHeadline.style.animation = "changeAnimation 1s ease-in-out";
+
+    setTimeout(() => {
+        changingHeadline.textContent = contentArray[index];
+        changingHeadline.style.color = colorArray[index];
+        index = (index + 1) % contentArray.length;
+    }, 300);
+}
+
+changeHeadline()
+
+setInterval(changeHeadline, 2000)
